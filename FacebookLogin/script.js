040d24466edf8b2c44bf62d14a57cd7f119c899b
Nato Tsertsvadze <76736484+Nato-tsrs@users.noreply.github.com>
2022-04-10 23:56:42 +0400

    Add files via upload

---
+++ FacebookLogin/script.js
@@ -0,0 +1,11 @@
+function create(){
+    var main2=document.getElementById("nav");
+    main2.style.display="inline";
+
+  
+    
+}
+function cancel(){
+    var main2=document.getElementById("nav");
+    main2.style.display="none";
+}
